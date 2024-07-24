c70aeb6569071c2737db8c1ef681a5a1415c3e04
Joseph Kariuki <joehene@gmail.com>
2024-07-24 15:20:05 +0300

    Added global pagination settings

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -152,6 +152,11 @@ MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
+
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [

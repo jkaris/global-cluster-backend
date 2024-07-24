24f52a61983ae2d071693a8dd4d9e4a310b1f7dd
Joseph Kariuki <joehene@gmail.com>
2024-07-24 16:53:18 +0300

    Refactored file validator

--- referrals/validators.py
+++ referrals/validators.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import ValidationError
 
 
-def validate_image_size(image):
+def validate_file_size(image):
     max_size = 10 * 1024 * 1024  # 10 MB
     if image.size > max_size:
-        raise ValidationError(f"Image file too large ( > {max_size} bytes )")
+        raise ValidationError(f"File too large ( > {max_size} bytes )")

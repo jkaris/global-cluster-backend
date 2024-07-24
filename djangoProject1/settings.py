71c235918694b300bde25b4bd6d3bc203ce428ec
Joseph Kariuki <joehene@gmail.com>
2024-07-24 14:47:38 +0300

    Created media url and root configuration

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -144,6 +144,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

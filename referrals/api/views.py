0996d466df0b5ae044d0a8215d405f8cd8b4110f
Joseph Kariuki <joehene@gmail.com>
2024-07-25 16:34:40 +0300

    Refactored support ticket viewset

--- referrals/api/views.py
+++ referrals/api/views.py
@@ -98,10 +98,10 @@ class ProductDetailAPIView(APIView):
 
 
 class SupportTicketViewSet(viewsets.ModelViewSet):
-    queryset = SupportTicket.objects.all()
+    # queryset = SupportTicket.objects.all()
     serializer_class = SupportTicketSerializer
     permission_classes = [permissions.IsAuthenticated]
-    http_method_names = ['get', 'post', 'put', 'patch', 'delete']
+    # http_method_names = ['get', 'post', 'put', 'patch', 'delete']
 
     def perform_create(self, serializer):
         serializer.save(submitted_by=self.request.user)
@@ -112,30 +112,7 @@ class SupportTicketViewSet(viewsets.ModelViewSet):
         for the currently authenticated user.
         """
         user = self.request.user
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(
-            serializer.data, status=status.HTTP_201_CREATED, headers=headers
-        )
-
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop("partial", False)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-
-        if getattr(instance, "_prefetched_objects_cache", None):
-            instance._prefetched_objects_cache = {}
-
-        return Response(serializer.data)
-
-    def perform_update(self, serializer):
-        serializer.save()
+        return SupportTicket.objects.filter(submitted_by=user)
 
 
 class UserRankingViewSet(viewsets.ModelViewSet):

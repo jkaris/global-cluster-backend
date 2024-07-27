fb022c9ce4e55f7a1d786045fe284d71a138e71a
Mohamed-code-marvel <mohaabdul568@gmail.com>
2024-07-27 07:32:25 +0300

    Added JWT token urls

--- useraccounts/urls.py
+++ useraccounts/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import IndividualProfileViewSet, CompanyProfileViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 router = DefaultRouter()
 router.register(r'individuals', IndividualProfileViewSet)
@@ -8,4 +12,6 @@ router.register(r'companies', CompanyProfileViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]

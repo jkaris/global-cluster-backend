b7d84ed41f5dbd4bcd88cce84f289f961c3c97ff
Mohamed-code-marvel <mohaabdul568@gmail.com>
2024-07-27 06:32:59 +0300

    Created user creation forms

--- useraccounts/admin.py
+++ useraccounts/admin.py
@@ -1,27 +1,40 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import CustomUser, IndividualProfile, CompanyProfile
+from .forms import UserCreationForm, UserChangeForm
 
 
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
     list_display = ('email', 'name', 'user_type', 'is_staff', 'is_active')
     list_filter = ('user_type', 'is_staff', 'is_active')
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        ('Personal Info', {'fields': ('name',)}),
-        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'user_type')}),
+        ('Personal Info', {'fields': ('name', 'user_type', 'profile_picture')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser')}),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'name', 'user_type', 'is_staff', 'is_active')}
+            'fields': (
+                'email', 'name', 'user_type', 'profile_picture', 'password1', 'password2', 'is_staff', 'is_active')}
          ),
     )
     search_fields = ('email',)
     ordering = ('email',)
 
 
-admin.site.register(CustomUser, CustomUserAdmin)
-admin.site.register(IndividualProfile)
+admin.site.register(CustomUser, UserAdmin)
+
+
+@admin.register(CompanyProfile)
+class CompanyProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'company_name', 'company_registration_number', 'phone_number', 'address', 'country')
+
+
+@admin.register(IndividualProfile)
+class IndividualProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'first_name', 'last_name', 'phone_number', 'address', 'country', 'state', 'city')

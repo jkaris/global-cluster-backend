42bfe0a0e115f23e26a3bd203ace81c5067c03b6
Mohamed-code-marvel <mohaabdul568@gmail.com>
2024-07-28 08:22:15 +0300

    removed unused import

--- referrals/views.py
+++ referrals/views.py
@@ -1,7 +1,6 @@
 import requests
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.generics import GenericAPIView
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import IsAuthenticated, AllowAny
